8d24477580e497860d06e72f38a062ecd29914ab
Chetan <chetanmittal39@gmail.com>
2025-06-23 13:05:37 +0530

    Detection of Loop Problems

--- Linked List/singly linked list/Medium/LengthOfLoop.cpp
+++ Linked List/singly linked list/Medium/LengthOfLoop.cpp
@@ -36,23 +36,30 @@ public:
 // Optimal Approach : Tortoise and Hair method
 class Solution {
 public:
+
+    int length(ListNode* slow , ListNode* fast){
+        int count = 1;
+        while(fast -> next != slow){
+            count++;
+            fast = fast -> next;
+        }
+        return count;
+    }
+    
     int findLengthOfLoop(ListNode *head) {
 
         ListNode* slow = head;
         ListNode* fast = head;
 
-        while(slow != fast){
+        while(fast != NULL && fast -> next != NULL){
             slow = slow -> next;
             fast = fast -> next -> next;
+
+            if(slow == fast){
+                return length(slow , fast);
+            }
         }
 
-        int length = 1;
-
-        while(fast -> next != slow){
-            length++;
-            fast = fast -> next;
-        }
-
-        return length;
+        return 0;
     }
 };
