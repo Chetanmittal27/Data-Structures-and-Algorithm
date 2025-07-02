bb5c75e243da6938286122a7f5dfb8e4b7fdc770
Chetan <chetanmittal39@gmail.com>
2025-07-02 19:51:59 +0530

    Learn Binary Tree Traversals

---
+++ Binary Trees/Basics/postOrder_Traversal.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    public:
+    Node(int d){
+        data = d;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+Node* createBinaryTree(Node* root){
+    cout << "Enter the root node" << endl;
+    int data;
+    cin >> data;
+
+    root = new Node(data);
+
+    if(data == -1){
+        return NULL;
+    }
+
+    cout << "Enter the node left to the node " << data << endl;
+    root -> left = createBinaryTree(root);
+    cout << "Enter the node right to the node " << data << endl;
+    root -> right = createBinaryTree(root);
+
+    return root;
+}
+
+void postOrder(Node* root){
+
+    if(root == nullptr){
+        return;
+    }
+
+    postOrder(root -> left);
+    postOrder(root -> right);
+    cout << root -> data << " ";
+}
+
+int main(){
+    Node* root = NULL;
+    root = createBinaryTree(root);
+
+    postOrder(root);
+    cout << endl;
+
+    return 0;
+}
