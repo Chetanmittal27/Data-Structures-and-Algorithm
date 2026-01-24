8943b702f671dff2d971acfe39a0c244b46b4c33
Chetan <chetanmittal39@gmail.com>
2026-01-24 17:27:00 +0530

    Adjacency List

--- Graphs/Representation of Graph/adjacency_list.cpp
+++ Graphs/Representation of Graph/adjacency_list.cpp
@@ -1,19 +1,35 @@
 #include<iostream>
 #include<vector>
+#include<unordered_map>
+
 using namespace std;
 
 int main(){
-
     int n , m;
-    cin >> n >> m;
+    cout << "Enter the number of vertices" << endl;
+    cin >> n;
+    cout << "Enter the number of edges" << endl;
+    cin >> m;
 
-    vector<int>lst(n+1);
+    unordered_map<int , vector<int>>mpp;
 
     for(int i=0;i<m;i++){
         int u , v;
         cin >> u >> v;
 
-        lst[u].push_back(v);
-        lst[v].push_back(u);
+        mpp[u].push_back(v);
+        mpp[v].push_back(u);
     }
+
+    for(auto &it : mpp){
+
+        cout << "For vertex " << it.first << endl;
+        for(auto ele : it.second){
+            cout << ele << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
 }
